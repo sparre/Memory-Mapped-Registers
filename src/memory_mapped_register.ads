64747b09220790d0737e0fad126bdbfbc50d39b6
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2014-10-10 11:31:56 +0200

    Adding a proper license to the repository.

--- src/memory_mapped_register.ads
+++ src/memory_mapped_register.ads
@@ -1,3 +1,10 @@
+--  <jacob@jacob-sparre.dk> wrote this. As long as you retain this
+--  notice you can do whatever you want with this stuff. If we meet
+--  some day, and you think this stuff is worth it, you can buy me a
+--  beer in return.
+--
+--  Jacob Sparre Andersen
+
 with System;
 generic
    type Element_Type is private;
--- src/packaged_register.adb
+++ src/packaged_register.adb
@@ -1,3 +1,10 @@
+--  <jacob@jacob-sparre.dk> wrote this. As long as you retain this
+--  notice you can do whatever you want with this stuff. If we meet
+--  some day, and you think this stuff is worth it, you can buy me a
+--  beer in return.
+--
+--  Jacob Sparre Andersen
+
 with Memory_Mapped_Register;
 procedure Packaged_Register is
    Some_Memory : String := "Hello world!";
